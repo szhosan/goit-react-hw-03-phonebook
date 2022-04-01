e759db5a18de7435a6a735b962895148af04db57
@s_zhsn <s.zhosan@gmail.com>
2022-04-01 13:22:45 +0300

    hw-2-react-phonebook-ready

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import s from './ContactsList.module.css';
 
 const ContactsList = ({ contacts, onDeleteContact, children }) => (
